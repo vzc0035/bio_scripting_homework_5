619707857a655f2b774c68ccd39359e5e5eefbb6
Victoria M. Coutts <vzc0035@auburn.edu>
2020-02-19 17:11:43 -0600

    Minor cosmetic changes

---
+++ part_1/fix_me.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Reading data from the user's input
+echo 'Enter a value for a : '
+read a
+echo 'Enter a value for b : ' 
+read b
+
+add=$((a + b))
+
+echo Addition of a and b are $add
+
+sub=$((a - b))
+echo Subtraction of a and b are $sub
+
+mul=$((a * b))
+echo Multiplication of a and b are $mul
+
+div=$((a / b))
+echo Division of a and b are $div
+
+mod=$((a % b))
+echo Modulus of a and b are $mod
+
+((++a))
+echo Increment operator when applied on "a" results into a = $a
+
+((--b))
+echo Decrement operator when applied on "b" results into b = $b
+
+((--c))
+echo What was the default value \ of c if its value is now $c\?
