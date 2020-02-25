1c1294508c31d5e97c1aac8bf442878eb078688f
Victoria M. Coutts <vzc0035@auburn.edu>
2020-02-25 12:22:36 -0600

    Prepared script for next hour

--- part_2/sys_info_COUTTS.sh
+++ part_2/sys_info_COUTTS.sh
@@ -10,7 +10,7 @@ users=who
 #system uptime
 uptime=uptime
 
-printf "This is the time and date: " > sys_info.txt
+printf "This is the time and date: " >> sys_info.txt
 $td >> sys_info.txt
 printf "These are the current logged-in users: " >> sys_info.txt
 $users >> sys_info.txt
